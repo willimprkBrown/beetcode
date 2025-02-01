cd533975425cb4a88e2b9e87639bb1d78b87d2dc
William Park <william_c_park@brown.edu>
2025-02-01 16:32:25 -0500

    css

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="grid">
-            <button>Match</button>
-            <button>Profile</button>
-       </div>
+        <div className="header-container">
+            <h1 className="header-title"> beetcode </h1>
+            <div className="grid">
+                <button>Match</button>
+                <button>Profile</button>
+            </div>
+        </div>
+       
     );
 };
 
