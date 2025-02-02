c2a992b37b67bc7259a918c61841bb4a3fd68435
havi-nguyen <113549784+havi-nguyen@users.noreply.github.com>
2025-02-02 01:52:58 -0500

    updating icons and colors

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
-import './Header.css';
-import React from 'react';
+import "./Header.css";
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className="header-container">
-            <h1 className="header-title">beetcode</h1>
-        </div>
-    );
+  return (
+    <div className="header-container">
+      <div className="header-title-container">
+        <img
+          className="header-icon"
+          src={`${process.env.PUBLIC_URL}/beet.png`}
+          alt="Beet Icon"
+        />
+        <h1 className="header-title">beetcode</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
